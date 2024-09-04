c9ab1ff77ac77e7d0e45163e00e3808b1122faa0
Hilal Ahmad <140879548+Hilal-11@users.noreply.github.com>
2024-09-05 00:36:14 +0530

    changes again

--- Binary Search Algorithm/implementationBinarySearch.cpp
+++ Binary Search Algorithm/implementationBinarySearch.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 
 int main() {
-    cout<<"Binary Search Algorithm"<<endl;
-    return 0;
+    int data[10] = {10 , 34 , 76 , 9 , 98 , 22 , 11, 92 , 66 , 64};
+
+    for(int i = 0; i < 10; i++)  {
+        cout<<data[i]<<"\t";
+    }
 }
